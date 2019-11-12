9b36594a426c45714047dbe02a623c4bd28cf282
qutaiba92 <hanaqtahq@yahoo.com>
2019-11-12 13:02:58 +0200

    updated

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # About-Me
+# About-Me
+# Project Title
+
+One Paragraph of General project information
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+## Built With
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
--- js/app.js
+++ js/app.js
@@ -4,44 +4,90 @@ console.log('Welcome to your website Mr.', name)
 alert('Welcome to your website Mr.' + name)
 
 var life = prompt('Do you love your life?');
-life = life.toLowerCase();
-while (life != 'yes' && life != 'no') {
-    life = prompt('wrong answer please choose yes or no');
+var lower = life.toLowerCase();
+switch (lower) {
+    case 'yes':
+    case 'y':
+        alert('your answer is.' + life)
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is.' + life)
+        break;
+    default:
+        alert('wrong answer please choose yes/y or no/n');
+        break;
 }
 //console.log('your answer is.', life)
-alert('your answer is.' + life)
 
 var live = prompt('Do you live alone?');
-live = live.toLowerCase();
-while (live != 'yes' && live != 'no') {
-    live = prompt('wrong answer please choose yes or no');
+var lower = live.toLowerCase();
+switch (lower) {
+    case 'yes':
+    case 'y':
+        alert('your answer is.' + live)
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is.' + live)
+        break;
+    default:
+        alert('wrong answer please choose yes/y or no/n');
+        break;
 }
 //console.log('your answer is.', live)
-alert('your answer is.' + live)
 
 var scholarship = prompt('Do you enjoy software development scholarship?');
-scholarship = scholarship.toLowerCase();
-while (scholarship != 'yes' && scholarship != 'no') {
-    scholarship = prompt('wrong answer please choose yes or no');
+var lower = scholarship.toLowerCase();
+switch (lower) {
+    case 'yes':
+    case 'y':
+        alert('your answer is.' + scholarship)
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is.' + scholarship)
+        break;
+    default:
+        alert('wrong answer please choose yes/y or no/n');
+        break;
 }
 //console.log('your answer is.', scholarship)
-alert('your answer is.' + scholarship)
 
 var money = prompt('Are you good at saving money?');
-money = money.toLowerCase();
-while (money != 'yes' && money != 'no') {
-    money = prompt('wrong answer please choose yes or no');
+var lower = money.toLowerCase();
+switch (lower) {
+    case 'yes':
+    case 'y':
+        alert('your answer is.' + money)
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is.' + money)
+        break;
+    default:
+        alert('wrong answer please choose yes/y or no/n');
+        break;
 }
 //console.log('your answer is.', money)
-alert('your answer is.' + money)
 
 var work = prompt('If you had the option to not work for the rest to your life, would you take it?');
-work = work.toLowerCase();
-while (work != 'yes' && work != 'no') {
-    work = prompt('wrong answer please choose yes or no');
+var lower = work.toLowerCase();
+switch (lower) {
+    case 'yes':
+    case 'y':
+        alert('your answer is.' + work)
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is.' + work)
+        break;
+    default:
+        alert('wrong answer please choose yes/y or no/n');
+        break;
 }
 //console.log('your answer is.', work)
-alert('your answer is.' + work)
+
 alert('Welcome to your website Mr.' + name)
 
 
